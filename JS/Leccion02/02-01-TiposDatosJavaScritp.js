6667c9c2ff889309bfbfbe674039e74808e49883
JGusM <jgusmei@gmail.com>
2021-11-07 11:07:31 -0300

    definir correctamente el nombre de una variable

--- JS/Leccion02/02-01-TiposDatosJavaScritp.js
+++ JS/Leccion02/02-01-TiposDatosJavaScritp.js
@@ -1,7 +1,11 @@
-let nombreCompleto = "Juan Perez";
+let nombreCompleto = " Juan Perez";
+let nombrecompleto = "Carlos Lara";
 console.log( nombreCompleto );
+console.log( nombrecompleto );
 
-let x, y;
-x = 10, y = 20;
-let z = x + y;
-console.log(z); 
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+//let 1nombreCompleto; no estpa permitido iniciar el nombre de una variable con números
+
+//let break = 10; break es palabra reservada y no se puede usar para declarar una variable
