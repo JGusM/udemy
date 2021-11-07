eb17bb12c99d9992aabf38b9187fed679883cfcf
JGusM <jgusmei@gmail.com>
2021-11-06 23:36:22 -0300

    concatenacion con numeros y palabras

--- JS/Leccion02/02-01-TiposDatosJavaScritp.js
+++ JS/Leccion02/02-01-TiposDatosJavaScritp.js
@@ -1,63 +1,17 @@
-/*
-Ejemplos de
-tipos de datos
-en JavaScript
-*/
-//Tipo de dato string
-var nombre = "Carlos";
-console.log(typeof nombre);
+var nombre = 'Juan';
+var apellido = 'Perez';
 
-nombre = 10.5;
-console.log(typeof nombre);
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
 
-//Tipo de dato numérico
-var numero = 1000;
-console.log(numero);
+var nombreCompleto2 = 'Carlos' + ' ' + 'Lara';
+console.log(nombreCompleto2);
 
-//Tipo de dato object
-var objeto = {
-    nombre: "Juan",
-    apellido: "Perez",
-    telefono: "55443322"
-}
-console.log(typeof objeto);
-
-//Tipo de dato boolean (true, false)
-var bandera = false;
-console.log(typeof bandera);
-
-//Tipo de dato function
-function miFuncion(){}
-console.log(typeof miFuncion);
-
-//Tipo de dato Symbol
-var simbolo = Symbol("mi símbolo");
-console.log(typeof simbolo);
-
-//Tipo clase es una function
-class Persona{
-    constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
-console.log(typeof Persona);
-
-//Tipo undefined
-var x;
-console.log(typeof x);
-
-x = undefined;
+var x = nombre + 2 + 4;
 console.log(x);
 
-//nul = ausencia de valor
-var y = null;
-console.log(typeof y);
+x = nombre + (2+4);
+console.log(x);
 
-var autos = ['BMW', 'Audi', 'Volvo'];
-console.log(autos);
-console.log(typeof autos);
-
-var z = '';
-console.log(z);
-console.log(typeof z);
+x = 2 + 4 + nombre;
+console.log(x);
