f90c7bab07a2dae6ba988d07872ef16a80c37ad7
JGusM <jgusmei@gmail.com>
2021-11-07 19:40:41 -0300

    convierte string a nro y ejercicio si puede votar con if e if  ternario

--- JS/Leccion03/03-01-OperadoresJavaScript.js
+++ JS/Leccion03/03-01-OperadoresJavaScript.js
@@ -1,6 +1,17 @@
-let resultado = (1>2) ? "verdadero" : "falso"; 
-console.log(resultado);
+let miNumero = "18";
 
-let numero = 110;
-resultado = ( numero % 2 == 0 ) ? "Número par" : "Número impar";
-console.log( resultado);
+//console.log(typeof miNumero);
+
+let edad = Number (miNumero);
+//console.log(typeof edad);
+if (edad >= 18){
+    console.log("Puede votar");
+}
+else{
+    console.log("Muy joven para votar");
+}
+
+(edad >= 18) ? console.log("Puede votar") : console.log("Muy joven para votar")
+
+let resultado = (edad >= 18) ? "Puede votar" : "Muy joven para votar"
+console.log(resultado)
