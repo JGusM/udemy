4968acb52c6560c7ec0941e54ccb06eca5ce8662
JGusM <jgusmei@gmail.com>
2021-11-06 23:17:42 -0300

    tipos de datos boolean, function, symbol, class, undefined, null

--- JS/Leccion02/02-01-TiposDatosJavaScritp.js
+++ JS/Leccion02/02-01-TiposDatosJavaScritp.js
@@ -21,3 +21,35 @@ var objeto = {
     telefono: "55443322"
 }
 console.log(typeof objeto);
+
+//Tipo de dato boolean (true, false)
+var bandera = false;
+console.log(typeof bandera);
+
+//Tipo de dato function
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+//Tipo de dato Symbol
+var simbolo = Symbol("mi símbolo");
+console.log(typeof simbolo);
+
+//Tipo clase es una function
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);
+
+//Tipo undefined
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(x);
+
+//nul = ausencia de valor
+var y = null;
+console.log(typeof y);
