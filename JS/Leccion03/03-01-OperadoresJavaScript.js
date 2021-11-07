5785021a7fa22be5e4a833290ac7b65955dda7b1
JGusM <jgusmei@gmail.com>
2021-11-07 14:29:53 -0300

    comparacion mayor o mayor igual y menor o menor igual

--- JS/Leccion03/03-01-OperadoresJavaScript.js
+++ JS/Leccion03/03-01-OperadoresJavaScript.js
@@ -1,8 +1,14 @@
-//Operadores de comparacion 
-let a = 3, b = 2, c = 3;
+let a = 3, b = 3, c = "3";
 
-let z = a != c; // se revisa el valor sin importar el tipo
+let z = a < b;
 console.log(z);
 
-z = a !== c; //revisa los valores pero tambien los tipos
+z = a <= b;
 console.log(z);
+
+z = a > b;
+console.log(z);
+
+z = a >= b;
+console.log(z);
+
