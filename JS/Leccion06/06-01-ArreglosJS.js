a0f5ca3170d28a0224b00dd95e6ea6c567186c1b
JGusM <jgusmei@gmail.com>
2021-11-09 13:28:04 -0300

    agregar elementos a un array push y con .length

--- JS/Leccion06/06-01-ArreglosJS.js
+++ JS/Leccion06/06-01-ArreglosJS.js
@@ -14,3 +14,11 @@ console.log(autos[1])
 
 autos.push("Audi");
 console.log(autos);
+
+console.log(autos.length);
+autos[autos.length] = "Cadillac";
+
+console.log(autos);
+
+autos[6] = "Porsche";
+console.log(autos);
