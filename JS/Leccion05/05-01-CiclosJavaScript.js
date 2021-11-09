303a6ff7fa05891daa931d8aa29f94f22ac740ec
JGusM <jgusmei@gmail.com>
2021-11-09 09:57:14 -0300

    CONTINUE

--- JS/Leccion05/05-01-CiclosJavaScript.js
+++ JS/Leccion05/05-01-CiclosJavaScript.js
@@ -1,6 +1,6 @@
- for ( let contador = 0 ; contador <= 10; contador ++){
-    if( contador % 2 == 0)
-     console.log(contador);
-     break;
-}
-console.log("Fin del ciclo for");
+for ( let contador = 0; contador <= 10; contador++){
+    if( contador % 2 !== 0){
+        continue; //ir a la siguiente iteracion
+    }
+    console.log(contador);
+ }
