d58bd3c0ba8b6b47a6fb5841c450190aa0704535
JGusM <jgusmei@gmail.com>
2021-11-09 09:46:30 -0300

    BREAK

--- JS/Leccion05/05-01-CiclosJavaScript.js
+++ JS/Leccion05/05-01-CiclosJavaScript.js
@@ -1,17 +1,6 @@
-/* let contador = 0;
-
-while( contador < 3 ){
-    console.log(contador);
-    contador++;
-}
-
-do{
+ for ( let contador = 0 ; contador <= 10; contador ++){
+    if( contador % 2 == 0)
      console.log(contador);
-     contador++;    
-    }while( contador < 3 );
-    */
-   
-   for ( let contador = 0; contador < 3 ; contador++ ){
-    console.log(contador);
+     break;
 }
-console.log("Fin ciclo for");
+console.log("Fin del ciclo for");
