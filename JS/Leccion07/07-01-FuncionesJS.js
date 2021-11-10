63a0cb4c9b3334d3b64b95926d3305f214fb87e8
JGusM <jgusmei@gmail.com>
2021-11-09 22:12:42 -0300

    sumo todos los nros de un argumento usando un for con arguments.length

--- JS/Leccion07/07-01-FuncionesJS.js
+++ JS/Leccion07/07-01-FuncionesJS.js
@@ -1,10 +1,10 @@
-// Declaracin Funcion de tipo Expresion
-let sumar = function (a = 4, b = 5){
-    console.log(arguments[0]);
-    console.log(arguments[1]);
-    console.log(arguments[2]);
-    return a + b + arguments[2];
-};
-
-resultado = sumar(3, 6, 7);
+let resultado = sumarTodo(5, 4, 13, 10, 9, 10, 11, 3);
 console.log(resultado);
+
+function sumarTodo(){
+    let suma = 0;
+    for (let i =0; i < arguments.length; i++){
+        suma += arguments[i]; // suma = suma + arguments[i]
+    }
+    return suma
+}
