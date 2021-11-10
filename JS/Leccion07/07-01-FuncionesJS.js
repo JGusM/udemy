65286ec112a0ae7ccb840adae70bf965bf7a718f
JGusM <jgusmei@gmail.com>
2021-11-09 21:13:42 -0300

    Funcion expresada

--- JS/Leccion07/07-01-FuncionesJS.js
+++ JS/Leccion07/07-01-FuncionesJS.js
@@ -6,3 +6,9 @@ return a + b;
 // Llamando a la funcion
 let resultado = miFuncion(2, 3);
 console.log(resultado);
+
+// Declaracin Funcion de tipo Expresion
+let sumar = function (a, b){ return a + b };
+
+resultado = sumar(1, 2);
+console.log(resultado);
