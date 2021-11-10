d40e5825cee1e9ad133977c837f1b900bd82f9f3
JGusM <jgusmei@gmail.com>
2021-11-09 21:49:43 -0300

    arrow function

--- JS/Leccion07/07-01-FuncionesJS.js
+++ JS/Leccion07/07-01-FuncionesJS.js
@@ -1,24 +1,9 @@
-// Declaracion de la funcion
-function miFuncion(a, b){
-    console.log(arguments.length);
-    return a + b;
-}
-
-// Llamando a la funcion
-let resultado = miFuncion(2, 3);
-console.log(resultado);
-
 // Declaracin Funcion de tipo Expresion
 let sumar = function (a, b){ return a + b };
 
 resultado = sumar(1, 2);
 console.log(resultado);
 
-(function (a, b){
-    console.log("Ejecutando la funcion: " + (a + b));
-})(3, 4);
-
-console.log(typeof miFuncion);
-
-var miFuncionTexto = miFuncion.toString();
-console.log(miFuncionTexto);
+const sumarFuncionTipoFlecha = (a, b) => a + b;
+resultado = sumarFuncionTipoFlecha(3, 5);
+console.log(resultado);
