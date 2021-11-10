f04060c8867474473c26cca4fba86878e4720f91
JGusM <jgusmei@gmail.com>
2021-11-09 23:16:14 -0300

    Paso por referencia

--- JS/Leccion07/07-01-FuncionesJS.js
+++ JS/Leccion07/07-01-FuncionesJS.js
@@ -8,5 +8,18 @@ function cambiarValor(a){
 //Paso por valor
 cambiarValor(x);//10
 console.log(x);
-console.log(a);
+//console.log(a);
 
+const persona = {
+    nombre: "Juan",
+    apellido: "Perez"
+}
+
+function cambiarValorObjeto(p1){
+    p1.nombre = "Carlos";
+    p1.apellido = "Lara"
+}
+
+//Paso por referencia
+cambiarValorObjeto( persona);
+console.log(persona);
