606665c2388fac50f7252730e94f6e7f0995e531
JGusM <jgusmei@gmail.com>
2021-11-12 22:45:38 -0300

    Agregar metodos a Objetos en JS

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -5,11 +5,15 @@ let persona = {
     nombre: "Juan",
     apellido: 'Perez',
     email: 'jperez@mail.com',
-    edad: 28
+    edad: 28,
+    nombreCompleto: function(){
+        return this.nombre + ' ' + this.apellido;
+    }
 }
 
 console.log(persona.nombre);
 console.log(persona.edad);
 console.log(persona.apellido);
+console.log(persona.nombreCompleto());
 
 console.log(persona);
