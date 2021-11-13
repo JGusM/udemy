e1b4329303faa367c5e899c5b7c2eb1868144613
JGusM <jgusmei@gmail.com>
2021-11-12 23:11:03 -0300

    acceder a las propiedades de los objetos en JS

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -1,6 +1,3 @@
-let x = 10;
-console.log(x.length);
-
 let persona = {
     nombre: "Juan",
     apellido: 'Perez',
@@ -12,15 +9,10 @@ let persona = {
 }
 
 console.log(persona.nombre);
-console.log(persona.edad);
-console.log(persona.apellido);
-console.log(persona.nombreCompleto());
+console.log(persona['apellido']);
 
-console.log(persona);
-
-let persona2 = new Object();
-persona2.nombre = 'Carlos';
-persona2.direccion = 'Saturno 15';
-persona2.tel = '55443322';
-
-console.log(persona2.tel);
+// for in Este es un ciclo for que se puede usar para recorrer un objeto
+for( nombrePropiedad in persona){
+    console.log( nombrePropiedad );
+    console.log( persona[nombrePropiedad] );
+}
