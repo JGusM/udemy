2f5555f3a3d67b5e6168aeebbb4332b5083200b8
JGusM <jgusmei@gmail.com>
2021-11-12 22:54:18 -0300

    crar objetos con new Object

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -17,3 +17,10 @@ console.log(persona.apellido);
 console.log(persona.nombreCompleto());
 
 console.log(persona);
+
+let persona2 = new Object();
+persona2.nombre = 'Carlos';
+persona2.direccion = 'Saturno 15';
+persona2.tel = '55443322';
+
+console.log(persona2.tel);
