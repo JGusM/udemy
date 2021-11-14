35ed7ed830f0609767ec5f0faf159cc8aa4b0578
JGusM <jgusmei@gmail.com>
2021-11-14 18:20:45 -0300

    Distintas formas de crear objetos

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -21,3 +21,52 @@ padre.nombre = 'Carlos';
 // aca muestra en pantalla que el cambio no afecto al objeto madre
 console.log( padre );
 console.log( madre );
+
+let miObjeto = new Object();
+let miObjeto2 = {};
+// Lo siguiente lo agregue yo
+console.log(miObjeto)
+console.log(miObjeto2)
+console.log(typeof miObjeto)
+console.log(typeof miObjeto2)
+
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola';
+// Lo siguiente lo agregue yo
+console.log(miCadena1)
+console.log(miCadena2)
+console.log(typeof miCadena1)
+console.log(typeof miCadena2)
+
+let miNumero = new Number(1);
+let miNumero2 = 1;
+// Lo siguiente lo agregue yo
+console.log( miNumero );
+console.log( miNumero2 );
+console.log( typeof miNumero );
+console.log( typeof miNumero2 );
+
+
+let miBoolean = new Boolean(false);
+let miBoolean2 = false;
+// Lo siguiente lo agregue yo
+console.log( miBoolean );
+console.log( miBoolean2 );
+console.log( typeof miBoolean );
+console.log( typeof miBoolean2 );
+
+let miArreglo1 = new Array();
+let miArreglo2 = [];
+// Lo siguiente lo agregue yo
+console.log( miArreglo1 );
+console.log( miArreglo2 );
+console.log( typeof miArreglo1 );
+console.log( typeof miArreglo2 );
+
+let miFuncion = new Function();
+let miFuncion2 = function(){};
+// Lo siguiente lo agregue yo
+console.log( miFuncion );
+console.log( miFuncion2 );
+console.log( typeof miFuncion );
+console.log( typeof miFuncion2 );
