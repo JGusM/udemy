bb7b8d97b3ae4f32695b7915ced002bc18663edd
JGusM <jgusmei@gmail.com>
2021-11-14 13:00:34 -0300

    agrega Metodo a un constructor de objetos

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -3,13 +3,17 @@ function Persona(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + ' ' + this.apellido;
+    }
 }
 
+// aca agrega el console para ver nombreCompleto
 let padre = new Persona('Juan', 'Perez', 'jperez@mail.com');
-console.log( padre );
+console.log( padre.nombreCompleto() );
 
 let madre = new Persona('Laura', 'Quintero', 'lquintero@mail.com');
-console.log( madre );
+console.log( madre.nombreCompleto() );
 
 // aca modifico el nombre del primer objeto
 padre.nombre = 'Carlos';
