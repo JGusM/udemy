a6e638aae89fcd3eec90fd4cd61742139bcc49df
JGusM <jgusmei@gmail.com>
2021-11-14 21:08:23 -0300

    uso de call

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -1,19 +1,18 @@
-//Funcion constructor de objetos de tipo Persona
-function Persona(nombre, apellido, email){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.nombreCompleto = function(){
+let persona1 = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    nombreCompleto: function(){
         return this.nombre + ' ' + this.apellido;
     }
 }
 
-Persona.prototype.tel = '44332211'
+let persona2 = {
+    nombre: 'Carlos',
+    apellido: 'Lara',
+}
 
-let padre = new Persona('Juan', 'Perez', 'jperez@mail.com');
-padre.tel = '11223344'
-console.log( padre.tel );
+// Uso de call para usar...
+// ...el metodo persona1.nombreCompleto conlos datos del persona2
+console.log( persona1.nombreCompleto() );
 
-let madre = new Persona('Laura', 'Quintero', 'lquintero@mail.com');
-madre.tel = '66889900'
-console.log( madre.tel );
+console.log(persona1.nombreCompleto.call( persona2 ));
