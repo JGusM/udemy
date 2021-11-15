02835bbb5f20e011011315d98837d4c8612181b3
JGusM <jgusmei@gmail.com>
2021-11-14 21:15:42 -0300

    paso de argumentos a call

--- JS/Leccion08/08-01.Objects.js
+++ JS/Leccion08/08-01.Objects.js
@@ -1,8 +1,8 @@
 let persona1 = {
     nombre: 'Juan',
     apellido: 'Perez',
-    nombreCompleto: function(){
-        return this.nombre + ' ' + this.apellido;
+    nombreCompleto: function(titulo, tel){//agrego los parametros titulo y tel
+        return titulo + ': ' + this.nombre + ' ' + this.apellido + ', ' + tel; //no le pone this porque se refiere a la funcion
     }
 }
 
@@ -13,6 +13,6 @@ let persona2 = {
 
 // Uso de call para usar...
 // ...el metodo persona1.nombreCompleto conlos datos del persona2
-console.log( persona1.nombreCompleto() );
+console.log( persona1.nombreCompleto('Lic.', '11223344') ); //aca pasa los argumentos de la funcion
 
-console.log(persona1.nombreCompleto.call( persona2 ));
+console.log(persona1.nombreCompleto.call( persona2, 'Ing', '6655443322' )); //aca pasa los argumentos de la funcion
